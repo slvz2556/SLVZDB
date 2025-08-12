2f6e094919ded688f834fc6a3804e6d65b138d61
Ashkan Selahvarzy <ashkan.slvz@outlook.com>
2025-08-12 20:24:40 +0330

    Add project files.

---
+++ Console.Test/Program.cs
@@ -0,0 +1,84 @@
+﻿using SLVZDB;
+
+// <---  SLVZ.DEV  --->
+
+var db = new myDB();
+
+//Add 3 users
+db.Append(new myModel
+{
+    ID = 1,
+    Name = "Ashkan",
+    Grade = 19.234,
+    IsActive = true,
+    Role = Role.Admin
+});
+db.Append(new myModel
+{
+    ID = 2,
+    Name = "Arash",
+    Grade = 21.992,
+    IsActive = true,
+    Role = Role.User
+});
+db.Append(new myModel
+{
+    ID = 3,
+    Name = "Baran",
+    Grade = 18.224452,
+    IsActive = false,
+    Role = Role.User
+});
+
+
+
+//Get all models
+var models = db.Get();
+
+
+//Change one of the models
+models.Find(x => x.ID == 3).IsActive = true;
+
+
+//Save that model
+db.Update(models.Find(x => x.ID == 3));
+
+
+//Delete model
+//db.Remove("3");
+
+
+//Find one item by it ID
+var model = db.Get(3);
+
+
+Console.WriteLine("Done");
+
+
+
+
+
+class myDB : DbContext<myModel>
+{
+    public override void Configuration()
+    {
+        SetConfig($"{Environment.CurrentDirectory}/slvz.db", "ID");
+                  //Database full path                      //Variable name to use as ID to find models
+    }
+}
+
+
+
+enum Role
+{
+    Admin,
+    User
+}
+class myModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public Role Role { get; set; }
+    public double Grade { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ SLVZDB/DbContext.cs
@@ -0,0 +1,371 @@
+﻿namespace SLVZDB;
+
+public abstract class DbContext<TModel>
+{
+    private string KeyName { get; set; }
+    private string FilePath { get; set; }
+    private Type ModelType { get; set; }
+
+    public abstract void Configuration();
+    public DbContext<TModel> SetConfig(string filepath, string keyname)
+    {
+        ModelType = typeof(TModel);
+        FilePath = filepath;
+        this.KeyName = keyname;
+
+        return this;
+    }
+
+    protected DbContext()
+    {
+        Configuration();
+
+        if (ModelType == null)
+            throw new InvalidOperationException("Model type must be set in Configuration().");
+
+        if (string.IsNullOrEmpty(KeyName) || string.IsNullOrEmpty(FilePath))
+            throw new InvalidOperationException("Both KeyName and FilePath must be set in Configuration().");
+    }
+
+
+
+    public void Append(TModel model)
+    {
+        if (model == null)
+            throw new ArgumentNullException(nameof(model));
+        if (model.GetType() != ModelType)
+            throw new InvalidOperationException($"This configuration only works with model type {ModelType.Name}.");
+
+
+        try
+        {
+            using FileStream file = new FileStream(FilePath, File.Exists(FilePath) ? FileMode.Append : FileMode.CreateNew, FileAccess.Write);
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                string str = "";
+                var properties = ModelType.GetProperties();
+
+                string key = ModelType.GetProperty(KeyName).GetValue(model).ToString();
+                if (key.Contains("\n"))
+                    throw new InvalidDataException("You cannot use Enter in key");
+
+                str += $"<db.{KeyName}>{key}</db.{KeyName}><db.br/>";
+
+                foreach (var prop in properties)
+                {
+                    if (prop.Name != KeyName)
+                    {
+                        string value = ModelType.GetProperty(prop.Name).GetValue(model).ToString();
+                        var type = prop.PropertyType;
+
+                        if (type == typeof(string))
+                            value = value.Replace("\n", "<db.break/>");
+
+                        str += $"<db.{prop.Name}>{value}</db.{prop.Name}><db.br/>";
+                    }
+                }
+                if (str.EndsWith("<db.br/>"))
+                    str = str.Remove(str.Length - 9, 8);
+
+                writer.WriteLine(str);
+
+                writer.Close();
+            }
+
+            file.Close();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public void Append(List<TModel> models)
+    {
+        if (models is null)
+            throw new ArgumentNullException(nameof(models));
+        if (models[^1]?.GetType() != ModelType)
+            throw new InvalidOperationException($"This configuration only works with model type {ModelType.Name}.");
+
+
+        try
+        {
+            using FileStream file = new FileStream(FilePath, File.Exists(FilePath) ? FileMode.Append : FileMode.CreateNew, FileAccess.Write);
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                foreach (var model in models)
+                {
+                    string str = "";
+                    var properties = ModelType.GetProperties();
+
+                    string key = ModelType.GetProperty(KeyName).GetValue(model).ToString();
+                    if (key.Contains("\n"))
+                        throw new InvalidDataException("You cannot use Enter in key");
+
+                    str += $"<db.{KeyName}>{key}</db.{KeyName}><db.br/>";
+
+                    foreach (var prop in properties)
+                    {
+                        if (prop.Name != KeyName)
+                        {
+                            string value = ModelType.GetProperty(prop.Name).GetValue(model).ToString();
+                            var type = prop.PropertyType;
+
+                            if (type == typeof(string))
+                                value = value.Replace("\n", "<db.break/>");
+
+                            str += $"<db.{prop.Name}>{value}</db.{prop.Name}><db.br/>";
+                        }
+                    }
+                    if (str.EndsWith("<db.br/>"))
+                        str = str.Remove(str.Length - 9, 8);
+
+                    writer.WriteLine(str);
+                }
+                writer.Close();
+            }
+
+            file.Close();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public List<TModel> Get()
+    {
+        if (ModelType == null)
+            throw new InvalidOperationException("Model type is not set.");
+
+        if (typeof(TModel) != ModelType)
+            throw new InvalidOperationException($"This configuration only works with model type {ModelType.Name}.");
+
+        List<TModel> objects = new List<TModel>();
+
+
+        if (!File.Exists(FilePath))
+            return objects;
+
+        using FileStream file = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+        using (var reader = new StreamReader(file))
+        {
+            while (reader.Peek() != -1)
+            {
+
+                string line = reader.ReadLine();
+                var instance = Activator.CreateInstance(ModelType);
+
+                foreach (var parameter in line.Split("<db.br/>"))
+                {
+                    foreach (var prop in ModelType.GetProperties())
+                    {
+                        if (parameter.StartsWith($"<db.{prop.Name}>") && !prop.PropertyType.IsEnum)
+                        {
+                            if (prop.PropertyType == typeof(string))
+                            {
+                                string value = parameter.Replace($"<db.{prop.Name}>", "").Replace($"</db.{prop.Name}>", "").Replace("<db.break/>", "\n");
+                                prop.SetValue(instance, value);
+                            }
+                            else
+                            {
+                                var value = Convert.ChangeType(parameter.Replace($"<db.{prop.Name}>", "").Replace($"</db.{prop.Name}>", ""), prop.PropertyType);
+                                prop.SetValue(instance, value);
+                            }
+                            break;
+                        }
+                        else if (parameter.StartsWith($"<db.{prop.Name}>") && prop.PropertyType.IsEnum)
+                        {
+                            object convertedValue = parameter.Replace($"<db.{prop.Name}>", "").Replace($"</db.{prop.Name}>", "");
+                            if (convertedValue is string strVal)
+                                convertedValue = Enum.Parse(prop.PropertyType, strVal);
+                            else
+                                convertedValue = Enum.ToObject(prop.PropertyType, convertedValue);
+
+                            prop.SetValue(instance, convertedValue);
+
+                            break;
+                        }
+                    }
+                }
+
+
+                //Add model to the list
+                objects.Add((TModel)Convert.ChangeType(instance, ModelType));
+            }
+            reader.Close();
+        }
+        file.Close();
+
+        return objects;
+    }
+
+    public TModel Get(dynamic Key)
+    {
+        if (ModelType == null)
+            throw new InvalidOperationException("Model type is not set.");
+
+        if (typeof(TModel) != ModelType)
+            throw new InvalidOperationException($"This configuration only works with model type {ModelType.Name}.");
+
+        var key = Activator.CreateInstance(ModelType.GetProperty(KeyName).PropertyType);
+        key = Convert.ChangeType(key, key.GetType());
+
+        var instance = Activator.CreateInstance(ModelType);
+
+        if (!File.Exists(FilePath))
+            return (TModel)Convert.ChangeType(instance, ModelType);
+
+        using FileStream file = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+        using (var reader = new StreamReader(file))
+        {
+            while (reader.Peek() != -1)
+            {
+
+                string line = reader.ReadLine();
+
+                if (line.StartsWith($"<db.{KeyName}>{key}</db.{KeyName}>"))
+                {
+                    foreach (var parameter in line.Split("<db.br/>"))
+                    {
+                        foreach (var prop in ModelType.GetProperties())
+                        {
+                            if (parameter.StartsWith($"<db.{prop.Name}>") && !prop.PropertyType.IsEnum)
+                            {
+                                if (prop.PropertyType == typeof(string))
+                                {
+                                    string value = parameter.Replace($"<db.{prop.Name}>", "").Replace($"</db.{prop.Name}>", "").Replace("<db.break/>", "\n");
+                                    prop.SetValue(instance, value);
+                                }
+                                else
+                                {
+                                    var value = Convert.ChangeType(parameter.Replace($"<db.{prop.Name}>", "").Replace($"</db.{prop.Name}>", ""), prop.PropertyType);
+                                    prop.SetValue(instance, value);
+                                }
+
+                                break;
+                            }
+                            else if (parameter.StartsWith($"<db.{prop.Name}>") && prop.PropertyType.IsEnum)
+                            {
+                                object convertedValue = parameter.Replace($"<db.{prop.Name}>", "").Replace($"</db.{prop.Name}>", "");
+                                if (convertedValue is string strVal)
+                                    convertedValue = Enum.Parse(prop.PropertyType, strVal);
+                                else
+                                    convertedValue = Enum.ToObject(prop.PropertyType, convertedValue);
+
+                                prop.SetValue(instance, convertedValue);
+
+                                break;
+                            }
+                        }
+                    }
+                    return (TModel)Convert.ChangeType(instance, typeof(TModel));
+                }
+            }
+            reader.Close();
+        }
+        file.Close();
+
+        return (TModel)Convert.ChangeType(instance, ModelType);
+    }
+
+    public void Remove(dynamic Key)
+    {
+        if (ModelType == null)
+            throw new InvalidOperationException("Model type is not set.");
+
+        var key = Activator.CreateInstance(ModelType.GetProperty(KeyName).PropertyType);
+        key = Convert.ChangeType(key, key.GetType());
+
+        if (File.Exists(FilePath))
+        {
+            using FileStream MainFile = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            using FileStream TmpFile = new FileStream(FilePath + ".tmp", FileMode.CreateNew, FileAccess.Write);
+
+            using StreamWriter writer = new StreamWriter(TmpFile);
+            using StreamReader reader = new StreamReader(MainFile);
+
+            while (reader.Peek() != -1)
+            {
+                string line = reader.ReadLine();
+                if (!line.StartsWith($"<db.{KeyName}>{key}</db.{KeyName}>"))
+                    writer.WriteLine(line);
+            }
+
+            reader.Close();
+            writer.Close();
+
+            MainFile.Close();
+            TmpFile.Close();
+
+            File.Copy(FilePath + ".tmp", FilePath, true);
+            File.Delete(FilePath + ".tmp");
+        }
+
+    }
+
+    public void Update(TModel model)
+    {
+        if (ModelType == null)
+            throw new InvalidOperationException("Model type is not set.");
+
+        if (model.GetType() != ModelType)
+            throw new InvalidOperationException($"This configuration only works with model type {ModelType.Name}.");
+
+
+        if (File.Exists(FilePath))
+        {
+            string key = ModelType.GetProperty(KeyName).GetValue(model).ToString();
+
+            using FileStream MainFile = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            using FileStream TmpFile = new FileStream(FilePath + ".tmp", FileMode.CreateNew, FileAccess.Write);
+
+            using StreamWriter writer = new StreamWriter(TmpFile);
+            using StreamReader reader = new StreamReader(MainFile);
+
+            while (reader.Peek() != -1)
+            {
+                string line = reader.ReadLine();
+                if (!line.StartsWith($"<db.{KeyName}>{key}</db.{KeyName}>"))
+                    writer.WriteLine(line);
+                else
+                {
+                    string str = "";
+                    var properties = ModelType.GetProperties();
+
+                    str += $"<db.{KeyName}>{key}</db.{KeyName}><db.br/>";
+
+                    foreach (var prop in properties)
+                    {
+                        if (prop.Name != KeyName)
+                        {
+                            string value = ModelType.GetProperty(prop.Name).GetValue(model).ToString();
+                            var type = prop.PropertyType;
+
+                            if (type == typeof(string))
+                                value = value.Replace("\n", "<db.break/>");
+
+                            str += $"<db.{prop.Name}>{value}</db.{prop.Name}><db.br/>";
+                        }
+                    }
+                    if (str.EndsWith("<db.br/>"))
+                        str = str.Remove(str.Length - 9, 8);
+
+                    writer.WriteLine(str);
+                }
+            }
+
+            reader.Close();
+            writer.Close();
+
+            MainFile.Close();
+            TmpFile.Close();
+
+            File.Copy(FilePath + ".tmp", FilePath, true);
+            File.Delete(FilePath + ".tmp");
+        }
+
+    }
+}
+
+//SLVZ
